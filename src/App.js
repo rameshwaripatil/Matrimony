132161859bdc0482e0255f197bc0889484a94794
Rameshwaripatil <rameshwaripatil68@gmail.com>
2023-10-04 19:19:39 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Header } from './Website/Components/Header'
 import { Footer } from './website/Components/Footer'
+import { Header } from './Website/Components/Header'
 
 const App = () => {
   return (
 
-    <div> <Header></Header>
+    <div>
+      <Header></Header>
     <Footer></Footer>
      </div>
 
