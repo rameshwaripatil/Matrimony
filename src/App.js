d7c7f115377ee010d54c9144a0bd0062b76c0791
mrx_sandy <sandipgadekar143@gmail.com>
2023-10-04 12:11:34 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Header } from './Components/Header'
+import { Footer } from './Components/Footer'
 
 const App = () => {
   return (
-    <div> <Header/> </div>
+    <div> <Header/>
+    <Footer/>
+     </div>
+
   )
 }
 
