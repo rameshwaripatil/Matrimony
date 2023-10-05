9a07b0fb6cdc0415dde530f057aa1de87ef107ef
mrx_sandy <sandipgadekar143@gmail.com>
2023-10-05 13:02:56 +0530

    home set

--- src/Website/Layout/Master.jsx
+++ src/Website/Layout/Master.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Components/Header'
 import { Footer } from '../Components/Footer'
+import { Home } from '../Components/Home'
 
 export const Master = () => {
   return (
     <div>
       <Header/>
+      <Home/>
       <Footer/>
 
     </div>
