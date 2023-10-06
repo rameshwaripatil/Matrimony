efbf95f7b97160b647673d4f338126452be7b30d
mrx_sandy <sandipgadekar143@gmail.com>
2023-10-06 12:28:21 +0530

    rcf add

--- src/Website/Layout/Master.jsx
+++ src/Website/Layout/Master.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from '../Components/Header'
 import { Footer } from '../Components/Footer'
 
-export const Master = () => {
+export const Master = ({Rcf}) => {
   return (
     <div>
       <Header/>
+      <Rcf></Rcf>
       <Footer/>
 
     </div>
