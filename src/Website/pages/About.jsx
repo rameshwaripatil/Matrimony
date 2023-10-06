535b7f8dfbb87d1a499fceae4c48692669dbb929
Asmitasaste <sasteasmita@gmail.com>
2023-10-06 12:30:01 +0530

    About

--- src/Website/pages/About.jsx
+++ src/Website/pages/About.jsx
@@ -2,7 +2,218 @@ import React from 'react'
 
 const About = () => {
   return (
-   <div>About</div>
+   <div>
+<section id="about">
+	<img src="img/33.jpg" width="100%"/>
+	</section>
+    <section id="about_heading_main" class="clearfix">
+ <div class="container">
+  <div class="row">
+   <div class="col-sm-12">
+    <div class="about_heading_main_1">
+	  <h5><a href="#">Home</a> <span>//</span> About Us</h5>
+	  <h1>About <span> Us</span> </h1>
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+<section id="about_inner">
+ <div class="container">
+  <div class="row">
+   <div class="col-sm-12 space_all">
+    <div class="col-sm-6 space_all">
+	 <div class="about_1">
+	  <h2>INTEGER NEC</h2>
+	 </div>
+	</div>
+	<div class="col-sm-6 space_all">
+	 <div class="about_2">
+	  <p><a href="#"><i class="fa fa-instagram"></i></a></p>	  
+	 </div>
+	</div>
+   </div>
+   <div class="col-sm-12 space_all">
+    <div class="about_3">
+	 <h4>Sed Dignissim Lacinia<br/> 
+         Vestibulum Lacinia Eget Nulla</h4>
+	</div>
+	<div class="col-sm-9">
+	 <div class="about_4">
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odionec odio.
+		Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. 
+		Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+		torquent per conubia nostra, per inceptos . Curabitursodales ligula in libero. 
+		Sed dignissim lacinia nunc.</p>
+	  
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odionec odio.
+		Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. 
+		Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+		torquent per conubia nostra, per inceptos . Curabitursodales ligula in libero. 
+		Sed dignissim lacinia nunc.</p>
+	 </div>
+	</div>
+	<div class="col-sm-3">
+	 <div class="about_5">
+	  <p><a href="#"><img src="img/55.jpg"/></a></p>
+	 </div>
+	</div>
+   </div>
+   <div class="col-sm-12">
+    <div class="about_4">
+	 <p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. 
+		Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p> 
+	 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odionec odio.
+		Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. 
+		Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+		torquent per conubia nostra, per inceptos . Curabitursodales ligula in libero. 
+		Sed dignissim lacinia nunc</p>
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+<section id="about_middle">
+ <div class="container">
+  <div class="row">
+   <div class="about_middle clearfix">
+    <div class="col-sm-6">
+	 <div class="about_middle_left clearfix">
+	   <h3>Why We Are ?</h3>
+	   <h2>Best Matrimony Website</h2>
+	   <hr/>
+	   <p> Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet  Mauris massa Vestibulum lacinia arcu eget nulla.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitursodales ligula in libero. Sed dignissim lacinia nunc.</p>
+	   <div class="about_middle_left_inner clearfix">
+	    <h4><i class="fa fa-check"></i>Essential Recruitments</h4>
+		<p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum<br/> imperdiet Mauris massa.</p>
+	   </div>
+	   <div class="about_middle_left_inner clearfix">
+	    <h4><i class="fa fa-check"></i>About Us</h4>
+		<p> Per inceptos himenaeos   ligula in libero Sed dignissim lacinia nunc.</p>
+	   </div>
+	   <div class="about_middle_left_inner clearfix">
+	    <h4><i class="fa fa-check"></i>Married Couple</h4>
+		<p> Nulla quis sem at nibh elementum imperdiet Mauris massa Vestibulum lacinia<br/> arcu eget nulla.</p>
+	   </div>
+	   <div class="about_middle_left_inner clearfix">
+	    <h4><i class="fa fa-check"></i>Followed People</h4>
+		<p>Fusce nec tellus sed augue semper porta. Mauris massa.Vestibulum lacinia arcu eget nulla taciti sociosqu ad litora torquent</p>
+	   </div>
+	   <div class="about_middle_left_inner clearfix">
+	    <h4><i class="fa fa-check"></i>Our Arrangements</h4>
+		<p>Vestibulum lacinia arcu eget nulla Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+	   </div>
+	   <div class="about_middle_left_inner clearfix">
+	    <h4><i class="fa fa-check"></i>The Last Word</h4>
+		<p> Conubia nostra, per inceptos himenaeos  Curabitursodales ligula in libero  Sed dignissim lacinia nunc.</p>
+	   </div>
+	 </div>
+	</div>
+	<div class="col-sm-6">
+	 <div class="about_middle_right">
+	 </div>
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+<section id="about_last">
+ <div class="container">
+  <div class="row">
+	   <div class="col-sm-12">
+		<div class="about_last">
+		  <h1><span>Our</span> Team Unity!</h1>
+		  <p>Vestibulum lacinia arcu eget nulla taciti sociosqu ad litora torquent.</p>
+		</div>
+	   </div>
+	   <div class="team col-sm-12  clearfix">
+		
+			<div class="col-sm-3 ">
+			 <div class="team_1 clearfix">
+			   <div class="grid clearfix">
+							<figure class="effect-jazz">
+								<img src="img/35.jpg" alt="img25"/>
+		
+			     </figure>
+							
+			  </div>
+			 </div>
+			 <div class="team_inner clearfix text-center">
+			 <h3>Praesent Mauris</h3>
+			 <h4>OWNER and Founder</h4>	
+             <ul>
+			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			 </ul>			 
+			</div>
+			</div>
+			<div class="col-sm-3">
+			 <div class="team_1 clearfix">
+			   <div class="grid clearfix">
+							<figure class="effect-jazz">
+								<img src="img/36.jpg" alt="img25"/>			
+			     </figure>
+							
+			  </div>
+			 </div>
+			 <div class="team_inner clearfix text-center">
+			 <h3>Semper Porta</h3>
+			 <h4>Director</h4>
+			 <ul>
+			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			 </ul>
+		     </div>
+			</div>
+			<div class="col-sm-3">
+			 <div class="team_1 clearfix">
+			   <div class="grid clearfix">
+							<figure class="effect-jazz">
+								<img src="img/37.jpg" alt="img25"/>			
+			     </figure>
+							
+			  </div>
+			 </div>
+			 <div class="team_inner clearfix text-center">
+			 <h3>Eget Nulla</h3>
+			 <h4>Manager</h4>
+			 <ul>
+			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			 </ul>
+			</div>
+			</div>
+			<div class="col-sm-3">
+			 <div class="team_1 clearfix">
+			   <div class="grid clearfix">
+							<figure class="effect-jazz">
+								<img src="img/38.jpg" alt="img25"/>			
+			     </figure>							
+			  </div>
+			 </div>
+			 <div class="team_inner clearfix text-center">
+			 <h3>Semper Porta</h3>
+			 <h4>Director</h4>
+             <ul>
+			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			 </ul>			 
+			</div>
+			</div>
+		   </div>
+  </div>
+ </div>
+</section>
+
+   </div>
   )
 }
 
