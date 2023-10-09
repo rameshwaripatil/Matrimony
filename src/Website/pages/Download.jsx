59ab53fe479de564e72ddd587fe556487fdffcc0
Asmitasaste <sasteasmita@gmail.com>
2023-10-09 14:08:24 +0530

    Dowanload

--- src/Website/pages/Download.jsx
+++ src/Website/pages/Download.jsx
@@ -5,8 +5,8 @@ const Download = () => {
         <section class="image__with--text__section section--padding">
             <div class="container">
                 <div class="row row-cols-md-2 row-cols-1  align-items-center">
-                    <div class="col"><div class="image__with--text__thumbnail">
-                        <img class="display-block" src="/static/media/slider2.6c5b2deb9eb3eae159ca.jpg" alt="drone-image"/>
+                    <div class="col-lg-6"><div class="image__with--text__thumbnail">
+                        <img class="display-block" src="img/wed12.jpg" alt="drone-image" style={{height:"500px",width:"450px"}}/>
                             </div></div><div class="col"><div class="image__with--text__content">
                                 <h3 class="image__with--text__title mb-18">Download our mobile app &amp; find start searching your life partner in a tap.</h3>
                                 <p class="image__with--text__desc mb-25">A marriage bureau is a business that helps people find suitable matches for marriage. This can be done through online services or by matching people based on their preferences and interests.</p>
@@ -16,13 +16,13 @@ const Download = () => {
                                         </li></ul>
                                         </div>
                                         <div class="image__with--text__content--footer d-flex">
-                                            <img class="display-block" src="/static/media/text-shape.b74012aa0a0c8e3dcb4f.webp" alt="text-shape"/>
+                                            <img class="display-block" src="img/logo.webp" alt="text-shape"/>
                                             </div>
                                             </div>
                                             </div>
                                             </div>
                                             </div>
-                                            <img class="image__with--text__position--shape display-block" src="/static/media/image-with-text-section-shape.0e2bc03fb89de87da95c.webp" alt="shape img"/>
+
                                             </section>
     )
 }
