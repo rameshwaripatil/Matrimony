a58a6f4214f8aefa404d3c86516c1c7d15231998
Asmitasaste <sasteasmita@gmail.com>
2023-10-09 11:48:18 +0530

    dowanload

--- src/Website/pages/Download.jsx
+++ src/Website/pages/Download.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Download = () => {
-  return (
-    <div>Download</div>
-  )
+    return (
+        <div class="container">
+            <div class="row row-cols-md-2 row-cols-1  align-items-center">
+                <div class="col"><div class="image__with--text__thumbnail">
+                    <img class="display-block" src="/static/media/slider2.6c5b2deb9eb3eae159ca.jpg" alt="drone-image" />
+                    </div></div><div class="col"><div class="image__with--text__content">
+                        <h3 class="image__with--text__title mb-18">Download our mobile app &amp; find start searching your life partner in a tap.</h3>
+                        <p class="image__with--text__desc mb-25">A marriage bureau is a business that helps people find suitable matches for marriage. This can be done through online services or by matching people based on their preferences and interests.</p>
+                        <div class="image__with--text__percent color-primary-2 mb-40"><ul><li class="image__with--text__percent--list mb-25">
+                            <span class="image__with--text__percent--top d-flex justify-content-between align-content-center">
+                                <h2>Download App Now !</h2></span></li></ul></div><div class="image__with--text__content--footer d-flex">
+                                    <img class="display-block" src="/static/media/text-shape.b74012aa0a0c8e3dcb4f.webp" alt="text-shape" /></div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+    )
 }
 
 export default Download
