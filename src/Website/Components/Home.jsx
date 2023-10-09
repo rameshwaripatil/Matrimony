a90dfd1cfecf0a8df81b3e4e592672c44184d6ad
mrx_sandy <sandipgadekar143@gmail.com>
2023-10-09 12:46:37 +0530

    set

--- src/Website/Components/Home.jsx
+++ src/Website/Components/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Download from '../pages/Download'
+import SuccessfullMarriges from '../pages/SuccessfullMarriges'
+import Services from '../pages/Services'
 
 export const Home = () => {
   return (
@@ -59,7 +62,7 @@ export const Home = () => {
           <div className="outdoor_2 clearfix">
             <div className="grid clearfix">
               <figure className="effect-apollo">
-                <img src="img/12.jpg" alt="img22" />
+                <img src="img/64.jpg" alt="img22" />
                 <figcaption>
                   <h2>Matrimonial <span>Website</span></h2>
                   <p> Duis sagittis ipsum   mauris  fusce nec tellus </p>
@@ -72,7 +75,9 @@ export const Home = () => {
     </div>
   </div>
 </section>
-<section id="arrange">
+<Services/>
+
+{/* <section id="arrange">
   <div className="container">
     <div className="row">
       <div className="col-sm-12">
@@ -115,163 +120,14 @@ export const Home = () => {
       </div>
     </div>
   </div>
-</section>
+</section> */}
+
+
+<SuccessfullMarriges/>
 
 
 
-
-
-<section id="services_home">
-  <div className="container">
-    <div className="row">
-      <div className="col-sm-12">
-        <div className="services_home">
-          <h1 className="text-center"><span>Our</span>  Services</h1>
-          <h2 className="text-center"><i className="fa fa-arrows-h" /></h2>
-        </div>
-      </div>
-      <ul className="timeline">
-        <li>
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-heading">
-              <img className="img-responsive" src="img/8.jpg" />        
-            </div>
-            <div className="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>             
-            </div>           
-            <div className="timeline-footer">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>
-        <li className="timeline-inverted">
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-heading">
-              <img className="img-responsive" src="img/9.jpg"  />
-            </div>
-            <div className="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-            <div className="timeline-footer">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-heading">
-              <img className="img-responsive" src="img/10.jpg" />              
-            </div>
-            <div className="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>              
-            </div>           
-            <div className="timeline-footer">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>        
-        <li className="timeline-inverted">
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-heading">
-              <img className="img-responsive" src="img/54.jpg"  />              
-            </div>
-            <div className="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>              
-            </div>            
-            <div className="timeline-footer">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-heading">
-              <img className="img-responsive" src="img/11.jpg"  />              
-            </div>
-            <div className="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>              
-            </div>            
-            <div className="timeline-footer">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>
-        <li className="timeline-inverted">
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-heading">
-              <img className="img-responsive" src="img/53.jpg"  />
-            </div>
-            <div className="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-            <div className="timeline-footer primary">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title id data-original-title="11 hours ago via Twitter" style={{cursor: 'pointer'}} /></a></div>
-          <div className="timeline-panel">
-            <div className="timeline-body">
-              <p><b>All the credits go to <a href="#">Rafamaciel</a></b></p>
-              <p>I only make it responsive and remove the empty spaces to be more like Facebook timeline!</p>
-            </div>
-            <div className="timeline-footer primary">
-              <a><i className="fa fa-facebook" /></a>
-              <a><i className="fa fa-twitter" /></a>
-              <a><i className="fa fa-google-plus" /></a>
-              <a><i className="fa fa-linkedin" /></a>
-              <a><i className="fa fa-rss" /></a>
-              <a className="pull-right">Continuar Lendo</a>
-            </div>
-          </div>
-        </li>
-        <li className="clearfix" style={{float: 'none'}} />		 
-      </ul>
-      <div className="services_home_main">
-        <h2 className="text-center"><i className="fa fa-arrows-h" /></h2>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section id="photo_gallery">
+{/* <section id="photo_gallery">
   <div className="container">
     <div className="row">
       <div className="col-sm-12">
@@ -286,7 +142,6 @@ export const Home = () => {
               <li data-target="#Carousel" data-slide-to={1} />
               <li data-target="#Carousel" data-slide-to={2} className />
             </ol>
-            {/* Carousel items */}
             <div className="carousel-inner">
               <div className="item active">
                 <div className="row">
@@ -294,25 +149,25 @@ export const Home = () => {
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/14.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/15.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/16.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
-                </div>{/*.row*/}
-              </div>{/*.item*/}
+                </div>
+              </div>
               <div className="item">
                 <div className="row">
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/17.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/18.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/19.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/20.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
-                </div>{/*.row*/}
-              </div>{/*.item*/}
+                </div>
+              </div>
               <div className="item">
                 <div className="row">
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/21.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/22.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/23.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
                   <div className="col-sm-3"><a href="#" className="thumbnail"><img src="img/24.jpg" alt="Image" style={{maxWidth: '100%'}} /></a></div>
-                </div>{/*.row*/}
+                </div>
               </div>
-            </div>{/*.carousel-inner*/}
+            </div>
             <a data-slide="prev" href="#Carousel" className="left carousel-control">‹</a>
             <a data-slide="next" href="#Carousel" className="right carousel-control">›</a>
           </div>
@@ -320,11 +175,11 @@ export const Home = () => {
       </div>
     </div>
   </div>
-</section>
+</section> */}
 
+<Download/>
 
-
-<div className="contact_home_main clearfix">
+{/* <div className="contact_home_main clearfix">
   <section id="contact_top">
     <div className="container">
       <div className="row">
@@ -404,7 +259,7 @@ export const Home = () => {
       </div>
     </div>	
   </section>
-</div>
+</div> */}
 
 
 
@@ -416,7 +271,7 @@ export const Home = () => {
 
 
 
-<section id="success">
+{/* <section id="success">
   <div className="container">
     <div className="row">
       <div className="col-sm-12">
@@ -468,7 +323,7 @@ export const Home = () => {
       </div>
     </div>
   </div>
-</section>
+</section> */}
 
 
 
--- src/Website/pages/Services.jsx
+++ src/Website/pages/Services.jsx
@@ -2,80 +2,70 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div><section id="about_heading">
-    <div class="container">
-     <div class="row">
-      <div class="col-sm-12">
-       <div class="about_heading">
-         <h5><a href="#">Home</a> <span></span>Our Services</h5>
-         <h1>Our <span>  Services</span> </h1>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
-       </div>
-       <div class="services clearfix">
-       <div class="col-sm-6 space_left">
-         <div class="services_inner">
-           <h2>Meeting</h2>
-           <h5>03 Feb 2015</h5>
-           <div class="services_inner_1 clearfix">
-            <div class="col-sm-6 space_left services_inner_1_inner">
-              <img src="img/40.jpg" width="100%"/>
+    <div>
+      <section class="services__section services__section--bg section--padding">
+        <div class="container">
+          <div class="section__heading text-center mb-50">
+            <h2 class="section__heading--maintitle text__secondary mb-10">Our Best Service</h2><p class="section__heading--desc"> Marriage bureaus can provide a range of services.</p>
             </div>
-            <div class="col-sm-6 space_left services_inner_1_inner_1">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
-            </div>
-           </div>
-         </div>
-       </div>
-       <div class="col-sm-6 space_left">
-         <div class="services_inner">
-           <h2>Date & Time</h2>
-           <h5>04 Feb 2016</h5>
-           <div class="services_inner_1 clearfix">
-            <div class="col-sm-6 space_left services_inner_1_inner">
-              <img src="img/41.jpg" width="100%"/>
-            </div>
-            <div class="col-sm-6 space_left services_inner_1_inner_1">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
-            </div>
-           </div>
-         </div>
-       </div>
-      </div>
-      <div class="services border_none_1 margin_top_1 clearfix">
-       <div class="col-sm-6 space_left">
-         <div class="services_inner">
-           <h2>Married People</h2>
-           <h5>04 Mar 2016</h5>
-           <div class="services_inner_1 clearfix">
-            <div class="col-sm-6 space_left services_inner_1_inner">
-              <img src="img/42.jpg" width="100%"/>
-            </div>
-            <div class="col-sm-6 space_left services_inner_1_inner_1">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
-            </div>
-           </div>
-         </div>
-       </div>
-       <div class="col-sm-6 space_left">
-         <div class="services_inner">
-           <h2>Single People</h2>
-           <h5>06 Jan 2016</h5>
-           <div class="services_inner_1 clearfix">
-            <div class="col-sm-6 space_left services_inner_1_inner">
-              <img src="img/43.jpg" width="100%"/>
-            </div>
-            <div class="col-sm-6 space_left services_inner_1_inner_1">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
-            </div>
-           </div>
-         </div>
-       </div>
-      </div>
-      </div>
-     </div>
+            <div class="services__inner">
+              <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-2 mb--n30">
+                <div class="col custom-col mb-30">
+                  <article class="services__card">
+                    <a class="services__card--link" href="/">
+                      <div class="services__card--topbar d-flex justify-content-between">
+                        <div class="services__card--icon mb-20">
+                          <img class="display-block services__card--primary__icon" src="data:image/jpeg" alt="services-icon" />
+                          <img class="display-block services__card--secondary__icon" src="/static/media/service-icon2-white.5db366b7e1f8eb308234.webp" alt="services-icon" />
+                          </div>
+                          <div class="services__card--number"><span class="services__card--number__text">1</span></div></div><div class="services__card--content"><h3 class="services__card--maintitle mb-15"> Profile Registrat</h3>
+                          <p class="services__card--desc mb-15">Individuals interested in finding a partner register with the marriage bureau by providing their per...</p>
+                          </div>
+                          </a>
+              
+                          </article>
+                          </div>
+                          <div class="col custom-col mb-30">
+                            <article class="services__card">
+                              <a class="services__card--link" href="/">
+                                <div class="services__card--topbar d-flex justify-content-between">
+                                  <div class="services__card--icon mb-20">
+                                    <img class="display-block services__card--primary__icon" src="data:image/jpeg" alt="services-icon"/>
+                                    <img class="display-block services__card--secondary__icon" src="/static/media/service-icon2-white.5db366b7e1f8eb308234.webp" alt="services-icon"/>
+                                    </div>
+    <div class="services__card--number"><span class="services__card--number__text">2</span>
     </div>
-   </section>
-   </div>
+    </div>
+    <div class="services__card--content"><h3 class="services__card--maintitle mb-15">Matchmaking</h3>
+    <p class="services__card--desc mb-15">The marriage bureau uses its database and resources to search for compatible matches based on the cr...</p>
+    </div>
+    </a>
+    </article>
+    </div>
+    <div class="col custom-col mb-30">
+      <article class="services__card">
+        <a class="services__card--link" href="/">
+      <div class="services__card--topbar d-flex justify-content-between">
+        <div class="services__card--icon mb-20">
+          <img class="display-block services__card--primary__icon" src="data:image/jpeg" alt="services-icon"/>
+        <img class="display-block services__card--secondary__icon" src="/static/media/service-icon2-white.5db366b7e1f8eb308234.webp" alt="services-icon"/>
+        </div>
+    <div class="services__card--number">
+      <span class="services__card--number__text">3</span>
+    </div>
+    </div>
+    <div class="services__card--content">
+      <h3 class="services__card--maintitle mb-15">Profile Sharing</h3>
+      <p class="services__card--desc mb-15">Once potential matches are found, the marriage bureau shares the profiles and relevant information w...</p>
+      </div>
+      </a>
+      </article>
+      </div>
+      </div>
+      </div>
+      </div>
+      </section>
+      </div>
   )
 }
 
