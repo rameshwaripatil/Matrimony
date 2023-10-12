c059d774bcdb74a34ab585ee87b999314dacd9cf
Rameshwaripatil <rameshwaripatil68@gmail.com>
2023-10-12 21:00:42 +0530

    bride done

--- src/Website/pages/Brides.jsx
+++ src/Website/pages/Brides.jsx
@@ -39,9 +39,9 @@ const Brides = () => {
 	   </div>
 	   <div class="team col-sm-12  clearfix">
 		
-			<div class="col-sm-4 col-lg-4 ">
-			 <div class="team_1 clearfix">
-       <div className="team__card--thumbnail" style={{height: 300}}>
+			<div class="col-sm-4 col-lg-4 " >
+			 <div class="team_1 clearfix"  >
+       <div className="team__card--thumbnail" style={{height: "300px"}}>
             <img className="team__card--thumbnail__img display-block " src="https://admin.royalmarriagebureau.com/uploads/userimg/1695346740.jpg" alt="team-thumb" style={{height: 300}} />
             </div>
        <table align="center" style={{marginTop: 10}}>
@@ -86,81 +86,161 @@ const Brides = () => {
                   </tbody>
                   </table>
 			 </div>
-			 {/* <div class="team_inner clearfix text-center">
-			 <h3>Praesent Mauris</h3>
-			 <h4>OWNER and Founder</h4>	
-             <ul>
-			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-			 </ul>			 
-			</div> */}
+			
 			</div>
 			<div class="col-sm-4 col-lg-4">
 			 <div class="team_1 clearfix">
-			   <div class="grid clearfix">
-							<figure class="effect-jazz">
-								<img src="img/36.jpg" alt="img25"/>			
-			     </figure>
-							
-			  </div>
+			 <div className="team__card--thumbnail" style={{height: "300px"}}>
+            <img className="team__card--thumbnail__img display-block " src="https://admin.royalmarriagebureau.com/uploads/userimg/16963447891.jpg" alt="team-thumb" style={{height: 300}} />
+            </div>
+       <table align="center" style={{marginTop: 10}}>
+              <tbody>
+                <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  
+                  <td>Birth Date</td>
+                  <td>:</td>
+                  <td>2000-03-19 </td>
+                </tr>
+                <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Height</td>
+                  <td>:</td>
+                  <td>5ft 6in - 167cm meter.</td>
+                </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Age </td>
+                  <td>:</td>
+                  <td>21 Years </td>
+                  </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Occupation </td>
+                  <td>:</td>
+                  <td>..</td>
+                  </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Education</td>
+                  <td>:</td>
+                  <td>B.s.c </td></tr><tr />
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>employed_in</td>
+                  <td>Not Working..</td></tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>income</td>
+                  <td>:</td>
+                  <td>/-</td>
+                  </tr><tr />
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Taluka</td>
+                  <td>:</td>
+                  <td>Not Assign..</td></tr>
+                  </tbody>
+                  </table>
 			 </div>
-			 <div class="team_inner clearfix text-center">
-			 <h3>Semper Porta</h3>
-			 <h4>Director</h4>
-			 <ul>
-			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-			 </ul>
-		     </div>
+			
 			</div>
 			<div class="col-sm-4  col-lg-4">
 			 <div class="team_1 clearfix">
-			   <div class="grid clearfix">
-							<figure class="effect-jazz">
-								<img src="img/37.jpg" alt="img25"/>			
-			     </figure>
-							
-			  </div>
+			 <div className="team__card--thumbnail" style={{height: "300px"}}>
+            <img className="team__card--thumbnail__img display-block " src="https://admin.royalmarriagebureau.com/uploads/userimg/16963449301.jpg" alt="team-thumb" style={{height: 300}} />
+            </div>
+       <table align="center" style={{marginTop: 10}}>
+              <tbody>
+                <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  
+                  <td>Birth Date</td>
+                  <td>:</td>
+                  <td>2002-03-15 </td>
+                </tr>
+                <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Height</td>
+                  <td>:</td>
+                  <td>5ft 6in - 167cm meter.</td>
+                </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Age </td>
+                  <td>:</td>
+                  <td>21 Years </td>
+                  </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Occupation </td>
+                  <td>:</td>
+                  <td>..</td>
+                  </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Education</td>
+                  <td>:</td>
+                  <td>B.s.c </td></tr><tr />
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>employed_in</td>
+                  <td>Not Working..</td></tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>income</td>
+                  <td>:</td>
+                  <td>/-</td>
+                  </tr><tr />
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Taluka</td>
+                  <td>:</td>
+                  <td>Not Assign..</td></tr>
+                  </tbody>
+                  </table>
 			 </div>
-			 <div class="team_inner clearfix text-center">
-			 <h3>Eget Nulla</h3>
-			 <h4>Manager</h4>
-			 <ul>
-			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-			 </ul>
-			</div>
+			
 			</div>
 			<div class="col-sm-4  col-lg-4">
-			 <div class="team_1 clearfix">
-			   <div class="grid clearfix">
-							<figure class="effect-jazz">
-								<img src="img/38.jpg" alt="img25"/>			
-			     </figure>							
-			  </div>
+			 <div class="team_1 clearfix " style={{marginTop:"20px"}}>
+			 <div className="team__card--thumbnail" style={{height: "300px"}}>
+            <img className="team__card--thumbnail__img display-block " src="https://admin.royalmarriagebureau.com/uploads/userimg/16958796841.jpg" alt="team-thumb" style={{height: 300}} />
+            </div>
+       <table align="center" style={{marginTop: 10}}>
+              <tbody>
+                <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  
+                  <td>Birth Date</td>
+                  <td>:</td>
+                  <td>2002-03-15 </td>
+                </tr>
+                <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Height</td>
+                  <td>:</td>
+                  <td>5ft 6in - 167cm meter.</td>
+                </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Age </td>
+                  <td>:</td>
+                  <td>21 Years </td>
+                  </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Occupation </td>
+                  <td>:</td>
+                  <td>..</td>
+                  </tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Education</td>
+                  <td>:</td>
+                  <td>B.s.c </td></tr><tr />
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>employed_in</td>
+                  <td>Not Working..</td></tr>
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>income</td>
+                  <td>:</td>
+                  <td>/-</td>
+                  </tr><tr />
+                  <tr style={{borderBottom: '1px solid rgb(204, 204, 204)'}}>
+                  <td>Taluka</td>
+                  <td>:</td>
+                  <td>Not Assign..</td></tr>
+                  </tbody>
+                  </table>
 			 </div>
-			 <div class="team_inner clearfix text-center"/>
-			 <h3>Semper Porta</h3>
-			 <h4>Director</h4>
-             <ul>
-			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-				  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-			 </ul>			 
-			</div>
+			 
 			</div>
 		   </div>
   </div>
-  
+  </div>
 </section>
  </div>
+
 
                           
 
