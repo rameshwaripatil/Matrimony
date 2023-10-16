86ac097566c9e402fad11615df8205f6fb3a1f48
Rameshwaripatil <rameshwaripatil68@gmail.com>
2023-10-16 12:35:47 +0530

    authentication done

---
+++ src/Website/Authentication/Authuser.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Authuser = () => {
+    const navigate = useNavigate();
+    const getToken = () => {
+      const tokenString = sessionStorage.getItem("token");
+      const userToken = JSON.parse(tokenString);
+      return userToken;
+    };
+
+    const getUser = () => {
+      const userString = JSON.parse(sessionStorage.getItem("user"));
+      return userString;
+    };
+
+    const saveToken = (user, token) => {
+        sessionStorage.setItem("token", JSON.stringify(token));
+        sessionStorage.setItem("user", JSON.stringify(user));
+      };
+
+    const [token, setToken] = useState(getToken());
+  const [user, setUser] = useState(getUser());
+    const http = axios.create({
+       
+        baseURL: "http://marriagebureau.ajspire.com/api",
+        
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const logout = () => {
+        sessionStorage.clear();
+        setToken(null);
+        setUser(null);
+        navigate("/");
+    
+      };
+      return {
+        setToken: saveToken,
+        token,
+        user,
+        http,
+        logout,
+      };
+}
+
+export default Authuser
