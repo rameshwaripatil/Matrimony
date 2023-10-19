2f283cb06e42b53b4ee4f8ffac8056217481d1c8
Rameshwaripatil <rameshwaripatil68@gmail.com>
2023-10-19 15:40:58 +0530

    register done

--- src/Website/Authentication/Register.jsx
+++ src/Website/Authentication/Register.jsx
@@ -1,161 +1,192 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Authuser from './Authuser';
 
 const Register = () => {
-  const[formData,setformData]=useState({
-    name:'',
-    email:'',
-    mob_no:'',
-    address:'',
-    password:'',
-    confirmpassword:''
-
-  });  
+  const [mothert,setmothert] = useState([]);
+  const [profile,setprofile] = useState([]);
+  const {http,token}=Authuser();
+  const MotherTounge =() => {
+    http.get(`get/mother_tounge`)
+    .then((response) => {
+      setmothert(response.data.data);
+      console.log(response.data.data);
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error('Error adding bride data:', error);         
+    });
+};
+const ProfileCreated =() => {
+  http.get(`get/profile_created_by`)
+  .then((response) => {
+    setprofile(response.data.data);
+    console.log(response.data.data);
+  })
+  .catch((error) => {
+    // Handle errors
+    console.error('Error adding bride data:', error);         
+  });
+};
+  
+  const [formData, setFormData] = useState({
+    name: '',
+    lastname: '',
+    email: '',
+    mob_no: '',
+    address: '',
+    password: '',
+    confirmpassword: '',
+  });
 
   console.log(formData);
-  const onInputChange=(e)=>{
-    setformData({...formData,[e.target.name]:e.target.value});
+
+  const onInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    fetch('http://marriagebureau.ajspire.com/api/front_user_register', {
+    fetch('  http://marriagebureau.ajspire.com/api/front_user_register/send/massage', {
       method: 'POST',
-      body: JSON.stringify(formData),  // Convert formData to JSON string
+      body: JSON.stringify(formData),
       headers: {
-        'Content-Type': 'application/json',  // Specify content type as JSON
-        // Include other headers if needed
+        'Content-Type': 'application/json',
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Request failed with status ${res.status}`);
+        }
+        return res.json(); // Attempt to parse JSON
+      })
       .then((data) => {
         console.log(data);
+        // Handle the JSON response data
       })
       .catch((error) => {
         console.log("Error", error);
+        // Handle the error, e.g., display an error message to the user
       });
   };
+  useEffect(() => {
+    MotherTounge();
+    ProfileCreated();
+  }, [token]);   
   return (
     <div>
-<div>
-  <section id="about_heading" className="about_register">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12">
-          <div className="about_heading">
-            <h5><a href="#">Home</a> <span>//</span>Register</h5>
-          </div>
+      
+      <div className="clearfix clear_1">
+        <div className="col-sm-6 register_right_inner_1 space_left">
+          <input
+            type="text"
+            className="form-control"
+            name="name" 
+            onChange={(e) => onInputChange(e)}
+            placeholder="First Name *"
+          />
+        </div>
+        <div className="col-sm-6 register_right_inner_1 space_left">
+          <input
+            type="text"
+            className="form-control"
+            name="lastname" 
+            onChange={(e) => onInputChange(e)}
+            placeholder="Last Name *"
+          />
         </div>
       </div>
-    </div>
-  </section>
-  <section id="register" className="clearfix">
-    <div className="register clearfix">
-      <div className="col-sm-5 space_left">
-        <div className="register_left">
-          <div className="card-image"><img src="https://images.unsplash.com/photo-1654156577076-e0350ba86cc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c291dGglMjBpbmRpYW4lMjB3ZWRkaW5nfGVufDB8fDB8fHww&w=1000&q=80" style={{height:"700px"}} width="100%" /></div>
+      <div className="clearfix clear_1">
+        <div className="col-sm-6 register_right_inner_1 space_left">
+          <input
+            type="text"
+            className="form-control"
+            name="email" 
+            onChange={(e) => onInputChange(e)}
+            placeholder="Email *"
+          />
+        </div>
+        <div className="col-sm-6 register_right_inner_1 space_left">
+          <input
+            type="text"
+            className="form-control"
+            name="mob_no" 
+            onChange={(e) => onInputChange(e)}
+            placeholder="Mobile *"
+          />
         </div>
       </div>
-      <div className="col-sm-7">
-        <div className="register_right">
-          <h3>Sign Up</h3>
-          <hr />
-          <div className="register_right_inner clearfix">
-            <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" onChange={(e)=>onInputChange(e)} placeholder="First Name *" />
-              </div>
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control"  onChange={(e)=>onInputChange(e)}placeholder="Last Name *" />
-              </div>
-            </div>
-            <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" onChange={(e)=>onInputChange(e)} placeholder="Email *" />
-              </div>
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" onChange={(e)=>onInputChange(e)} placeholder="Mobile *" />
-              </div>
-            </div>
-            <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" onChange={(e)=>onInputChange(e)} placeholder="Location *" />
-              </div>
-              {/* <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" onChange={(e)=>onInputChange(e)} id="cal_1" placeholder="Date of Birth" />
-              </div>
-            </div>
-            <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <select className="form-control">
-                  <option>Search For</option>
-                  <option>Lorem</option>
-                  <option>Ipsum </option>
-                  <option>Dolor</option>
-                  <option>Amet</option>
-                  <option>Consectetur </option>
-                  <option>Elit</option>
-                </select>
-              </div>
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <select className="form-control">
-                  <option value="Religion">Religion</option>
-                  <option>Hindu</option>
-                  <option>Muslim </option>
-                  <option>Christan</option>
-                  <option>Sikh</option>
-                  <option>Buddha </option>
-                </select>
-              </div>
-            </div>
-            <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" placeholder="Caste" />
-              </div>
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <select className="form-control">
-                  <option value="Gender">Gender</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </div>
-            </div>
-            <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <select className="form-control">
-                  <option value="Marital Status">Marital Status</option>
-                  <option value="Married">Married</option>
-                  <option value="Single">Single</option>
-                </select>
-              </div>
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <select className="form-control">
-                  <option value="Gender">Upload Photo</option>
-                  <option value="Male">Lorem</option>
-                  <option value="Female">Ipsum</option>
-                </select>
-              </div>
-            </div> */}
-            {/* <div className="clearfix clear_1">
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <input type="text" className="form-control" placeholder="Captcha Code" />
-              </div>
-              <div className="col-sm-6 register_right_inner_1 space_left">
-                <h4>FBGRE</h4>
-              </div>
-            </div> */}
-            <div className="clearfix col-sm-12 clear_2">
-            <button type="submit" className="btn btn-primary btn-block" onClick={(e)=>onSubmit(e)}>Sign Up</button>
-
-            </div>
-          </div>
+      <div className="clearfix clear_1">
+        <div className="col-sm-6 register_right_inner_1 space_left">
+          <input
+            type="text"
+            className="form-control"
+            name="address" 
+            onChange={(e) => onInputChange(e)}
+            placeholder="address"
+          />
         </div>
+          <div className="col-sm-6 register_right_inner_1 space_left">
+            <label class="contact__form--label">Mother Tounge</label>
+            <select class="contact__form--input" name="mother_toungue"  onChange={(e) => onInputChange(e)}>
+              <option value="">Choose...</option>
+              {mothert.map((el)=>
+              (
+                <option value={el.mother_tounges_id}>{el.mothertounge}</option>
+              ))
+
+              }
+              </select></div>
+        </div>
+        <div class="col-lg-6 col-md-6">
+          <div class="contact__form--list mb-20">
+            <label class="contact__form--label">Date of Birth</label>
+          <input class="contact__form--input" placeholder="" name="date_of_birth" type="date"/></div>
+          <div class="contact__form--list mb-20">
+            <label class="contact__form--label">Profile Created By</label>
+            
+            <select class="contact__form--input" name="profile_created_by" onChange={(e) => onInputChange(e)}>
+              <option value="">Choose...</option>
+              {
+                profile.map((el)=>(
+                  <option value={el.profile_created_id}>{el.profilecreated}</option>
+                ))
+              }
+              </select></div></div>
+        <div className="clearfix clear_1">
+        <div className="col-sm-6 register_right_inner_1 space_left">
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            onChange={(e) => onInputChange(e)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div className="col-sm-6 register_right_inner_1 space_left">
+        
+          <input
+            type="password"
+            className="form-control"
+            name="confirmpassword"
+            onChange={(e) => onInputChange(e)}
+            placeholder="Confirm Password"
+          />
+        </div>
+        </div>
+      
+      <div className="clearfix col-sm-12 clear_2">
+        <button
+          type="submit"
+          className="btn btn-primary btn-block"
+          onClick={(e) => onSubmit(e)}
+        >
+          Sign Up
+        </button>
       </div>
+     
     </div>
-    </div>
-  </section></div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Register
+export default Register;
